361bcc059d57e1f2a73917f0cd26c9f3c85a26f8
Miguel Madurga Flores <miguel.madurga@gmail.com>
2024-09-10 10:56:07 -0400

    fixed README

--- src/BetaDecayUtils.jl
+++ src/BetaDecayUtils.jl
@@ -24,10 +24,15 @@ calculateqbetashellmodel(groundstateparent,groundstatechild,zparent,aparent,zchi
 calculate the beta-decay Q value using shell model calculation of the ground state energies
 
 groundstateparent:absolute energy of the parent ground state in MeV
+
 groundstatechild:absolute energy of the child ground state in MeV 
+
 zparent: Z of the parent
+
 aparent: A of the parent
+
 zchild: Z of the child
+
 aparent: A of the child
 
 """
