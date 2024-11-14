f7e0ba0209a0f1c7a378c6b0261d4218fc804513
Miguel Madurga Flores <miguel.madurga@gmail.com>
2024-11-14 16:54:22 -0500

    V1.5 added gammafit

--- src/BetaDecayUtils.jl
+++ src/BetaDecayUtils.jl
@@ -2,6 +2,8 @@ module BetaDecayUtils
 
 export calculateqbetashellmodel,logf,logftfromib,calculateT12,childActivity, grandChildActivity,chainActivity,nPenetrability,logftfrombgt,calculateIb,calculateBGT,ecoulomb
 
+using LsqFit
+
 ## β decay utilities
 
 """
@@ -83,11 +85,11 @@ function chainActivity(t,A,λ,n)
 
     for i in 1:n
 
-    product=λ[i]
+        product=λ[i]
 
-    for j in 1:n if j!=i product*=λ[j]/(λ[j]-λ[i]);  end end
+        for j in 1:n if j!=i product*=λ[j]/(λ[j]-λ[i]);  end end
     
-    sum+=product*exp(-λ[i]*t)
+        sum+=product*exp(-λ[i]*t)
 
     end
     
@@ -364,6 +366,69 @@ function nPenetrability(x,mass::Vector,Lorb)
     elseif (Lorb==6.)
         return p6
     end
+
+end
+
+"""
+gammafit(data,xlow,xhigh,param::Vector,n=nothing,lowerbounds=nothing,upperbounds=nothing)
+
+Gamma photopeak fit of a histogram using a gaussian distribution with linear background. 
+Returns a three element list containing: parameters from fit; standard error of the parameters; fitting function f(x,param)
+
+data:           histogram in the [energy,counts] format
+xlow:           low energy cut for the fit
+xhigh:          high energy cut for the fit
+param:          initial fit parameters vector (must be 3*n+1). Format: [background-constant,background-slope,area1,centroid1,sigma1,area2,centroid2,sigma2,...]
+n:              number of peaks to fit (default=1)
+lowerbounds:    lower parameter bounds (optional). same format as param
+upperbounds:    upper parameter bounds (optional). same format as param
+
+Example: fit a gamma line at 988 keV
+
+xlow,xhigh,param=980,1000,[600,0.05,1000,988,0.8]
+p,s,f = gammafit(data,xlow,xhigh,param)
+
+to print the result of the fit parameters
+
+for (i,val) in enumerate(p)
+    println("P$i = ",val,"($(s[i]))")
+end
+
+to plot the result of the fit use the returned function f with the optimized parameters p
+
+plot(e->f(e,p),xlow,xhigh)
+
+"""
+function gammafit(data,xlow,xhigh,param::Vector,n=nothing,lowerbounds=nothing,upperbounds=nothing)
+  
+    if length(param)!=n*3+2 throw(DomainError(length(param), "must have [3*number of photopeak + 2] parameters")) end
+    if isnothing(lowerbounds) if upperbounds!==nothing throw(DomainError(lowerbounds,"both lower and upper bounds must be defined")) end end
+    if isnothing(upperbounds) if lowerbounds!==nothing throw(DomainError(lowerbounds,"both lower and upper bounds must be defined")) end end
+
+    if isnothing(n) n=1 end 
+
+
+    fitfunctions = [(e,p)->p[1].+ p[2].*e .+ p[3].*(1 ./sqrt.(2π.*p[5].^2)).*exp.(-0.5.*(e.-p[4]).^2 ./p[5].^2) , 
+                    (e,p)->p[1].+ p[2].*e .+ p[3].*(1 ./sqrt.(2π.*p[5].^2)).*exp.(-0.5.*(e.-p[4]).^2 ./p[5].^2) .+ p[6].*(1 ./sqrt.(2π.*p[8].^2)).*exp.(-0.5.*(e.-p[7]).^2 ./p[8].^2)]
+
+
+    fit=nothing
+    
+    if isnothing(lowerbounds) && isnothing(upperbounds)
+        if n==1 fit=curve_fit(fitfunctions[1],data[data[:,1].<xhigh .&& data[:,1].>xlow,1],data[data[:,1].<xhigh .&& data[:,1].>xlow,2],param)
+        elseif n==2 fit=curve_fit(fitfunctions[n],data[data[:,1].<xhigh .&& data[:,1].>xlow,1],data[data[:,1].<xhigh .&& data[:,1].>xlow,2],param)
+        else throw(DomainError(n, "number of photopeaks must be 2 or less"))
+        end
+    else 
+        if n==1 fit=curve_fit(fitfunctions[1],data[data[:,1].<xhigh .&& data[:,1].>xlow,1],data[data[:,1].<xhigh .&& data[:,1].>xlow,2],param,lower=lowerbounds,upper=upperbounds)
+        elseif n==2 fit=curve_fit(fitfunctions[n],data[data[:,1].<xhigh .&& data[:,1].>xlow,1],data[data[:,1].<xhigh .&& data[:,1].>xlow,2],param,lower=lowerbounds,upper=upperbounds)
+        else throw(DomainError(n, "number of photopeaks must be 2 or less"))
+        end
+    end
+
+    sigma=stderror(fit)
+
+    return fit.param,sigma,fitfunctions[n]
 
 end
 
